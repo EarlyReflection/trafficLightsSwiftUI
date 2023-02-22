a814f5f6fc62f984ffc0cf8849e1139dd902fa32
Vladimir Dvornikov <echobizarre@gmail.com>
2023-02-22 14:54:56 +0200

    Initial Commit

---
+++ trafficLightsSwiftUI/trafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightsSwiftUIApp.swift
+//  trafficLightsSwiftUI
+//
+//  Created by Vladimir Dvornikov on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
